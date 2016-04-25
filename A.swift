7d11fe269c8bc71e9f177508539a44206e42b802
355154430 <355154430@qq.com>
2016-04-25 21:24:44 +0800

    1.0
    1.0

---
+++ A.swift
@@ -0,0 +1,41 @@
+//
+//  A.swift
+//  asdasd
+//
+//  Created by King on 16/4/25.
+//  Copyright © 2016年 King. All rights reserved.
+//
+
+import UIKit
+var a3: Double=0
+var ar: String=""
+class A: UIViewController {
+    
+    @IBOutlet weak var L1: UILabel!
+    @IBOutlet weak var L2: UILabel!
+    @IBOutlet weak var L3: UILabel!
+    @IBOutlet weak var L4: UILabel!
+    @IBOutlet weak var L5: UILabel!
+    @IBOutlet weak var L6: UILabel!
+    //or = L3.text
+    @IBAction func zuo(sender: UIButton) {
+        ar = L3.text!;
+        //a3 = ((ar) as NSString).DoubleValue
+        a3 = ((ar) as NSString).doubleValue
+        //L3.text = L3.text ++;
+        // L3.text = "" ((operand) as NSString).doubleValue
+        //a3 = L3.text;
+        //a3 = ((or) as NSString).IntValue
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
---
+++ App.swift
@@ -0,0 +1,13 @@
+//
+//  App.swift
+//  asdasd
+//
+//  Created by King on 16/4/25.
+//  Copyright © 2016年 King. All rights reserved.
+//
+
+import UIKit
+
+class App: NSObject {
+
+}
