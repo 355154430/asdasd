1af464d08840ab26ea14ed4d13e6b57b333eb240
355154430 <355154430@qq.com>
2016-05-05 11:04:05 +0800

    2.2
    2.2

--- A.swift
+++ A.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-var a3: Double=0
+var a3=0;
 var ar: String=""
 class A: UIViewController {
     
@@ -18,17 +18,22 @@ class A: UIViewController {
     @IBOutlet weak var L5: UILabel!
     @IBOutlet weak var L6: UILabel!
     //or = L3.text
-    @IBAction func zuo(sender: UIButton) {
+    @IBAction func zuo1(sender: AnyObject) {
         ar = L3.text!;
+    }
+    //@IBAction func zuo(sender: UIButton) {
+        
+      //  ar = L3.text!;
         //a3 = ((ar) as NSString).DoubleValue
-        a3 = ((ar) as NSString).doubleValue
-        a3 = a3+1;
+        //a3 = ((ar) as NSString).IntValue
+        //a3++
+        //to string
         
         //L3.text = L3.text ++;
         // L3.text = "" ((operand) as NSString).doubleValue
         //a3 = L3.text;
         //a3 = ((or) as NSString).IntValue
-    }
+    //}
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
